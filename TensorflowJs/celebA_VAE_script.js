f85adc0a73d0d822d3fce25ec4903c1dcd27f8e4
호균 임 <jellyho@o365.yonsei.ac.kr>
2023-01-12 16:01:33 +0900

    jkj

--- TensorflowJs/celebA_VAE_script.js
+++ TensorflowJs/celebA_VAE_script.js
@@ -6,3 +6,33 @@ function loadFile(input) {
     //이미지 source 가져오기
     img.src = URL.createObjectURL(file);
 };
+
+traits = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair'
+    , 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male'
+    , 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes', 'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns'
+    ,'Smiling','Straight_Hair','Wavy_Hair','Wearing_Earrings','Wearing_Hat','Wearing_Lipstick','Wearing_Necklace','Wearing_Necktie','Young']
+
+train_values = {}
+sliders = {}
+slider_ps = {}
+
+function resetTraits() {
+    for (let i = 0; i < traits.length; i++) {
+        train_values[traits[i]] = 0;
+        var slider = document.getElementById(traits[i]);
+        sliders[traits[i]] = slider;
+        var sliderp = document.getElementById('vec_' + traits[i]);
+        slider_ps[traits[i]] = sliderp;
+        slider.addEventListener('input', (event) => {
+            var value = (event.target.value - 50) / 10;
+            slider_ps[event.target.id].innerText = value;
+            train_values[event.target.id] = value;
+        });
+    }
+    return true;
+}
+
+resetTraits();
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
