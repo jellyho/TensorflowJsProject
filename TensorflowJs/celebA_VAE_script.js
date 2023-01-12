2bd8e71adcf7b4c1a0cb428de1736aefc6714747
jellyho <jellyho@naver.com>
2023-01-13 01:29:53 +0900

    f

--- TensorflowJs/celebA_VAE_script.js
+++ TensorflowJs/celebA_VAE_script.js
@@ -1,13 +1,19 @@
 img = document.getElementById('upload');
+canvas = document.getElementById("constructed");
 
-const encoder = tf.loadGraphModel("https://raw.githubusercontent.com/jellyho/TensorflowJs/master/TensorflowJs/MNIST_VAE_DECODER/model.json");
-const decoder = tf.loadGraphModel("https://raw.githubusercontent.com/jellyho/TensorflowJs/master/TensorflowJs/MNIST_VAE_DECODER/model.json");
+const encoder = tf.loadGraphModel("https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/celebA_VAE_encoder/model.json");
+const decoder = tf.loadGraphModel("https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/celebA_VAE_decoder/model.json");
+
+var latent;
+var constructed;
 
 function loadFile(input) {
     var file = input.files[0];	//선택된 파일 가져오기
 
     //이미지 source 가져오기
     img.src = URL.createObjectURL(file);
+    encode();
+    decode();
 };
 
 traits = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair'
@@ -36,4 +42,21 @@ function resetTraits() {
 }
 
 resetTraits();
-fetch('trait_vectors.json').then((response) => response.json()).then((json) => console.log(json));
+fetch('https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/trait_vectors.json').then((response) => response.json()).then((json) => console.log(json));
+
+function encode()
+{
+    var input = tf.image.resizeBilinear(tf.browser.fromPixels(img), [128, 128]);
+    input = tf.reshape(input, [1, 128, 128, 3]);
+    input = tf.div(input, 255.0);
+    encoder.then(function (encoder) {latent = encoder.predict(input);});
+    console.log(latent);
+}
+
+function decode()
+{
+    var input = tf.reshape(latent[0], [1, 400]);
+    decoder.then(function (decoder) {constructed = decoder.predict(input);});
+    constructed = tf.reshape(constructed, [128, 128, 3]);
+    tf.browser.toPixels(constructed, canvas);
+}
