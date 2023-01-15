b05be59bd62385e77fb9a2c2340f6c0fba686127
호균 임 <jellyho@o365.yonsei.ac.kr>
2023-01-15 17:50:01 +0900

    hello

--- TensorflowJs/celebA_VAE_script.js
+++ TensorflowJs/celebA_VAE_script.js
@@ -1,5 +1,6 @@
 img = document.getElementById('upload');
 canvas = document.getElementById("constructed");
+state = document.getElementById("state");   
 
 const encoder = tf.loadGraphModel("https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/celebA_VAE_encoder/model.json");
 const decoder = tf.loadGraphModel("https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/celebA_VAE_decoder/model.json");
@@ -13,9 +14,17 @@ function loadFile(input) {
 
     //이미지 source 가져오기
     img.src = URL.createObjectURL(file);
-    encode();
-    decode();
+    console.log('image loaded');
+    setTimeout(function () { encode();}, 1000);
+    setTimeout(function () { decode();}, 2000);
 };
+
+function setExampleImage(obj) {
+    console.log('image loaded');
+    img.src = obj.src;
+    setTimeout(function () { encode(); }, 1000);
+    setTimeout(function () { decode(); }, 2000);
+}
 
 traits = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair'
     , 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male'
@@ -43,36 +52,45 @@ function resetTraits() {
     return true;
 }
 
-resetTraits();
-fetch('https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/trait_vectors.json').then((response) => response.json()).then((json) => trait_vectors=json);
 
 function update_traits() {
     var new_latent = tf.clone(latent[0]);
     for (let i = 0; i < traits.length; i++) {
         var trait = tf.tensor(trait_vectors[traits[i]]);
         trait = trait.mul(train_values[traits[i]]);
-        trait = tf.reshape(trait, [1, 400]);      
+        trait = tf.reshape(trait, [1, 400]);
         new_latent = new_latent.add(trait);
     }
-    console.log(new_latent.print());
+    var d = latent[0].norm();
+    new_latent = new_latent.div(new_latent.norm()).mul(d);
     decode_new(new_latent);
 }
 
-function encode()
-{
+function encode() {
+    state.innerText = "encoding...";
+    state.style = "width : 25%";
     var input = tf.image.resizeBilinear(tf.browser.fromPixels(img), [128, 128]);
     input = tf.reshape(input, [1, 128, 128, 3]);
     input = tf.div(input, 255.0);
-    encoder.then(function (encoder) {latent = encoder.predict(input);});
-    console.log(latent);
+    encoder.then(function (encoder) {
+        latent = encoder.predict(input);
+        console.log(latent[0].print());
+    });
+    state.innerText = "encoded";
+    state.style = "width : 50%";
 }
 
-function decode()
-{
+function decode() {
+    state.innerText = "decoding...";
+    state.style = "width : 75%";
     var input = tf.reshape(latent[0], [1, 400]);
-    decoder.then(function (decoder) {constructed = decoder.predict(input);});
-    constructed = tf.reshape(constructed, [128, 128, 3]);
-    tf.browser.toPixels(constructed, canvas);
+    decoder.then(function (decoder) {
+        constructed = decoder.predict(tf.reshape(input, [1, 400]));
+        constructed = tf.reshape(constructed, [128, 128, 3]);
+        tf.browser.toPixels(constructed, canvas);
+    });
+    state.innerText = "decoded";
+    state.style = "width : 100%";
 }
 
 function decode_new(latent) {
@@ -81,3 +99,6 @@ function decode_new(latent) {
     constructed = tf.reshape(constructed, [128, 128, 3]);
     tf.browser.toPixels(constructed, canvas);
 }
+
+resetTraits();
+fetch('https://raw.githubusercontent.com/jellyho/TensorflowJsProject.github.io/master/TensorflowJs/trait_vectors.json').then((response) => response.json()).then((json) => trait_vectors=json);
